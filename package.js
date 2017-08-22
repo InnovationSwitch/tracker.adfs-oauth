30ac80f9fad035e16ad30d1d733b9dc5d2645d1e
Werner Bihl <wernerbihl@gmail.com>
2017-08-22 03:24:42 -0700

    Removed versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'innovationswitch:tracker.adfs-oauth',
-  version: '0.0.7',
+  version: '0.0.8',
   summary: 'Oauth2 authentication for Tracker',
   git: 'https://github.com/InnovationSwitch/tracker.adfs-oauth',
   documentation: 'README.md'
