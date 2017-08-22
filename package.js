6397c63f486c69ef301b86e5e9e83bff468d23e7
Werner Bihl <wernerbihl@gmail.com>
2017-08-22 03:02:39 -0700

    Tweak

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'innovationswitch:tracker.adfs-oauth',
-  version: '0.0.6',
+  version: '0.0.7',
   summary: 'Oauth2 authentication for Tracker',
   git: 'https://github.com/InnovationSwitch/tracker.adfs-oauth',
   documentation: 'README.md'
